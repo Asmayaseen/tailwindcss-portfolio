0349e5926fc52ee89811739ae07471652147f673
Asmayaseen <asmayaseen9960@gmail.com>
2024-11-15 11:57:21 +0500

    first commit

---
+++ app/constants/index.ts
@@ -0,0 +1,127 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 14",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Linkedin",
+    src: "/linkedinIcon.png",
+  },
+ 
+  {
+    name: "Github",
+    src: "/githubIcon.png",
+  },
+];
+export const Projects = [
+ 
+ 
+  {
+    title: "React & CSS State property Website",
+    text: "it is a project that demonstrates the use of React and CSS to create dynamic and responsive websites, focusing on state management and styling.",
+    src: "/Real%20Estate%20-%20Google%20Chrome%2011_11_2024%205_44_34%20PM.png",
+  },
+  {
+    title: "next.js & Tailwindcss food website",
+    text: "it is a project that demonstrates the using Next.js and Tailwind CSS to create  responsive websites, focusing on state management and styling.",
+    src: "/Create%20Next%20App%20-%20Google%20Chrome%2011_6_2024%2011_54_04%20AM.png",
+  },
+  {
+    title: "Next.js & Tailwindcss Portfolio website",
+    text: "Next.js & Tailwindcss Portfolio: A modern, responsive portfolio created using Next.js and Tailwind CSS for a sleek, professional look.",
+    src: "/Asma%20Khan%20-%20Google%20Chrome%2011_6_2024%2012_03_02%20PM.png",
+  },
+  {
+    title: "React & CSS Portfolio",
+    text: "React & CSS Portfolio: A stylish and interactive portfolio built using React and CSS for an engaging user experience.",
+    src: "/Create%20Next%20App%20-%20Google%20Chrome%2011_6_2024%2011_59_31%20AM.png",
+  },
+];
+
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
